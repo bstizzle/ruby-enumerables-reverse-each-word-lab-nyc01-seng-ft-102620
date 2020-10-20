23b17b2e39fd62aa590db213b62751dd1bb37bec
Benjamin Stein <whereyouben@gmail.com>
2020-10-20 23:14:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   words = sentence.split(" ")
   revSentence = ""
   words.each do |element|
-    revSentence += element.reverse
+    revSentence += "#{element.reverse} "
   end 
   revSentence
 end 
